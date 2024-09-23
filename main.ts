0285a3704341a902823b606b272cd3b9dd458f6a
Rafa Escalante <rafa.escalante@gmail.com>
2024-09-23 00:58:04 -0600

    hola

--- main.ts
+++ main.ts
@@ -1,134 +1,133 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, MarkdownView, Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class ClassSchedulePlugin extends Plugin {
+    async onload() {
+        this.addCommand({
+            id: 'insert-class-schedule',
+            name: 'Insert Class Schedule',
+            hotkeys: [
+                {
+                    modifiers: ["Ctrl"],
+                    key: "F5",
+                },
+            ],
+            editorCallback: (editor: Editor, view: MarkdownView) => {
+                // Class schedule data
+                const classSchedule = [
+                    {
+                        day: 'Monday',
+                        startTime: '13:00',
+                        endTime: '14:50',
+                        className: 'COSC 1437 02 PROGRAMMING FUNDAMENTALS II',
+                        duration: 110
+                    },
+                    {
+                        day: 'Monday',
+                        startTime: '19:30',
+                        endTime: '21:20',
+                        className: 'COSC 1436 61 PROGRAMMING FUNDAMENTALS I',
+                        duration: 110
+                    },
+                    {
+                        day: 'Tuesday',
+                        startTime: '13:00',
+                        endTime: '14:50',
+                        className: 'COSC 1436 04 PROGRAMMING FUNDAMENTALS I',
+                        duration: 110
+                    },
+                    {
+                        day: 'Tuesday',
+                        startTime: '17:30',
+                        endTime: '18:40',
+                        className: 'COSC 2336 61 PROGRAMMING FUNDAMENTALS III',
+                        duration: 70
+                    },
+                    {
+                        day: 'Wednesday',
+                        startTime: '13:00',
+                        endTime: '14:50',
+                        className: 'COSC 1437 02 PROGRAMMING FUNDAMENTALS II',
+                        duration: 110
+                    },
+                    {
+                        day: 'Wednesday',
+                        startTime: '19:30',
+                        endTime: '21:20',
+                        className: 'COSC 1436 61 PROGRAMMING FUNDAMENTALS I',
+                        duration: 110
+                    },
+                    {
+                        day: 'Thursday',
+                        startTime: '13:00',
+                        endTime: '14:50',
+                        className: 'COSC 1436 04 PROGRAMMING FUNDAMENTALS I',
+                        duration: 110
+                    },
+                    {
+                        day: 'Thursday',
+                        startTime: '17:30',
+                        endTime: '18:40',
+                        className: 'COSC 2336 61 PROGRAMMING FUNDAMENTALS III',
+                        duration: 70
+                    },
+                ];
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+                // Function to convert time string to minutes
+                function timeStringToMinutes(timeStr: string): number {
+                    let [hours, minutes] = timeStr.split(':').map(Number);
+                    return hours * 60 + minutes;
+                }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+                // Get current date and time
+                let currentDate = new Date();
+                let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+                let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+                let currentDayName = daysOfWeek[currentDate.getDay()];
+                let currentMonthName = months[currentDate.getMonth()];
+                let currentDay = currentDate.getDate();
+                let currentTimeMinutes = currentDate.getHours() * 60 + currentDate.getMinutes();
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+                // Filter classes for today
+                let todayClasses = classSchedule.filter(c => c.day === currentDayName);
 
-	async onload() {
-		await this.loadSettings();
+                // Find the class that started before or at current time and has the latest start time
+                let selectedClass = null;
+                let latestStartTime = -1;
+                for (let c of todayClasses) {
+                    let classStartTime = timeStringToMinutes(c.startTime);
+                    if (classStartTime <= currentTimeMinutes && classStartTime > latestStartTime) {
+                        latestStartTime = classStartTime;
+                        selectedClass = c;
+                    }
+                }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+                // If no class has started yet today, find the next class
+                if (selectedClass === null) {
+                    let earliestStartTimeAfter = 24 * 60; // Max possible time
+                    for (let c of todayClasses) {
+                        let classStartTime = timeStringToMinutes(c.startTime);
+                        if (classStartTime > currentTimeMinutes && classStartTime < earliestStartTimeAfter) {
+                            earliestStartTimeAfter = classStartTime;
+                            selectedClass = c;
+                        }
+                    }
+                }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+                // Prepare the text to insert
+                let headerText = '';
+                if (selectedClass === null) {
+                    headerText = `## ${currentDayName}, ${currentMonthName} ${currentDay}, No classes today.`;
+                } else {
+                    headerText = `## ${currentDayName}, ${currentMonthName} ${currentDay}, ${selectedClass.startTime} ${selectedClass.className}`;
+                }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+                // Insert the formatted text as a header followed by two newlines
+                editor.replaceSelection(`${headerText}\n\n`);
+            }
+        });
+    }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    onunload() {
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    }
 }
